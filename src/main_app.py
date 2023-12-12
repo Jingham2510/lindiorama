3976223b2795a9f6b9777ef7590c6e8d88e77556
Joe <77728651+Jingham2510@users.noreply.github.com>
2023-12-12 10:13:57 +0000

    Logic Update - Comment Update
    -Added coord matching to match updated design
    -Updated Commenting
    -Updated idle screen logic
    - Updated Screensaver image logic

--- src/main_app.py
+++ src/main_app.py
@@ -15,10 +15,13 @@ import platform
 The main applet that controls the raspberry pi for the Alaris Linwave Diorama
 """
 
+#The main app - a child of the "tk" as this tends to be standard to tkinter applets
 class app(tk.Tk):
+    #The controller for the application
     def __init__(self):
 
-        #Check the 
+        #Check the platform - determines the filepath for where the cwd is
+        #OS.cwd() isn;t used here, as the code is run on startup by the raspberry pi from the root directory (the code is not stored there ATM)
         if "Windows" in platform.platform():
             print("Windows system")
             self.base_filepath = "P:/Joe/Python Scripts/diorama_app"
@@ -26,16 +29,16 @@ class app(tk.Tk):
             print("Raspbian system")
             self.base_filepath = "/home/pi/diorama_app"
 
-
+        #Create the window
         tk.Tk.__init__(self)
-        #self.protocol("WM_DELETE_WINDOW", self.on_close)
+        #Setup the control flags 
         self.closed_flag = 0
-        #Set the CWD correctly (i.e. up one)
+        
 
 
         #Setup the window
         self.title = ("Linwave Diorama App")
-        self.geometry("576x317")
+        
 
         #Configure the main window
         self.grid_columnconfigure(0, weight=1)                 
@@ -44,9 +47,11 @@ class app(tk.Tk):
         #Determine the starting frame     
         self._frame = None
         self.switch_frame(Start_Window)
+        #Start the timer
         self.start_time = time.time()
 
         #Setup so escape key closes the program
+        #DEBUG only - normally device in kiosk mode so won't have peripherals plugged in
         self.bind("<Escape>", self.on_close)
 
         #Hide the window bar and go fullscreen after 500ms
@@ -57,55 +62,60 @@ class app(tk.Tk):
     #Switches to the desired frame window
     def switch_frame(self, frame_class, *, arg = None):
         
+        #Set the start page flag 
         self.start_flag = 0
         
-        #Create the class correctly
+        #Determines which frame to go to
         match arg:
-            #Sector overview page
-            
-            case ("MARINE" | "AVIATION" | "DEFENCE" | "INDUSTRIAL" | "MEDICAL" | "SPACE") as s:
+            #Sector overview page            
+            case ("MARINE" | "AVIATION" | "DEFENCE" | "INDUSTRIAL" | "MEDICAL" | "SATCOM") as s:
                 print("overview page")
                 new_frame = frame_class(self, s)
                 
             
-            #Incase the capabilities and services page is different
-            case("Capabilities") as s :
+            #Futureprrofing for the possibility that the capabilities page is different
+            case("CAPABILITIES") as s :
                 print("capabilities page")
                 new_frame = frame_class(self, s)
                 
-
+            #Go to the home page
             case("Return"):
                 print("Home page")
                 new_frame = frame_class(self)                   
 
+            #Go to the specified page 
             case _:
                 new_frame = frame_class(self)     
                 
               
         
-        #Destroys the current window
+        #Destroys the current window (as long as it exists)
         if self._frame is not None:
             self._frame.destroy()
-        #Swaps to the next one 
+        
+
+        #Creates the new frame and places it 
         self._frame = new_frame
         self._frame.grid(row = 0, column = 0, sticky = "NESW")
 
-    #Get the current idle time on the page
+
+    #Get the current idle time on the page (i.e. how long the page has been open)
     def get_idle_time(self):
         return int(time.time() - self.start_time)
     
-    #Set the flag so that the program knows to close
+    #Set the flag so that the program knows the app has been closed
     def on_close(self, event):
         self.destroy()
         self.closed_flag = 1
 
 
 
-
+#The start page
 class Start_Window(tk.Frame):
     #Startup of the GUI - Start Window
     def __init__(self, master):             
                
+        #Create the window and set the relevant flags
         tk.Frame.__init__(self, master)
         self.master = master
         self.master.start_time = time.time()
@@ -113,13 +123,14 @@ class Start_Window(tk.Frame):
 
         
         
-        #The filepath where the screensavers are saved
-        #On the Pi : /home/pi/diorama_app/images/Screemsavers/
+        #The filepath where the screensavers are saved      
         ssavers_filepath = self.master.base_filepath + "/images/Screensavers/"
 
-        self.screensavers = [ f"{ssavers_filepath}{x}" for x in os.listdir(ssavers_filepath)]
+        #Generate a list of filepaths to loads the images from - based on all the images in the file 
+        self.screensavers = [ f"{ssavers_filepath}{x}" for x in os.listdir(ssavers_filepath) if x[-3:] in ["png", "jpg"]]
         
 
+        #Loads the first image in the generated list 
         self.curr_ssaver_index = 0
         display = Image.open(self.screensavers[self.curr_ssaver_index])
         display = display.resize((self.master.winfo_width(), self.master.winfo_height()))
@@ -129,10 +140,10 @@ class Start_Window(tk.Frame):
         self.curr_image.photo = display
         
         
-
+        #Sets the start time for the loaded image
         self.start_menu_time = time.time()
 
-       
+        #Binds mouse click 1 (or touchscreen click) to the menu swap
         self.curr_image.bind("<Button-1>", self._start_action)
 
 
@@ -141,18 +152,22 @@ class Start_Window(tk.Frame):
         print("changing page")
         self.master.switch_frame(Main_Window)
 
-
+    #Loops through the screensavers
     def _next_image(self):
         print("Next slide")
 
+        #Sets the start time
         self.start_menu_time = time.time()
 
+
+        #Generates the next index
         if self.curr_ssaver_index == len(self.screensavers) - 1:
             self.curr_ssaver_index = 0            
         else:
             self.curr_ssaver_index = self.curr_ssaver_index + 1
             
 
+        #Loads the next image
         display = Image.open(self.screensavers[self.curr_ssaver_index])
         display = display.resize((self.master.winfo_width(), self.master.winfo_height()))
         display = ImageTk.PhotoImage(display)
@@ -161,10 +176,12 @@ class Start_Window(tk.Frame):
         self.curr_image.photo = display        
         self.curr_image.bind("<Button-1>", self._start_action)
 
+
+
       
 
     
-    #Return the time since the last screensaver cjagem
+    #Return the time since the last screensaver changed
     def start_menu_tot_time(self):
         return int(time.time() - self.start_menu_time)
 
@@ -173,7 +190,7 @@ class Start_Window(tk.Frame):
 
   
 
-
+#The main overview window
 class Main_Window(tk.Frame):
 
     #Load the main page 
@@ -185,8 +202,10 @@ class Main_Window(tk.Frame):
         self.master = master
         self.master.start_time = time.time()
     
+        #Determine the filepath for the menu screen image
         overview_fp = self.master.base_filepath + "/images/menus/HomeScreen.png"
 
+        #Load the menu screen image
         img = Image.open(overview_fp)
         img2 = img.resize((self.master.winfo_width(), self.master.winfo_height()))
         display = ImageTk.PhotoImage(img2)        
@@ -194,6 +213,7 @@ class Main_Window(tk.Frame):
         curr_image.grid(row=0, column=0, sticky="NEWS")
         curr_image.resized_photo = display
 
+        #Set the mouse to be bound to changing the screen
         curr_image.bind("<Button-1>", self._open_overview)
 
         
@@ -203,28 +223,53 @@ class Main_Window(tk.Frame):
     #Open an overview page 
     def _open_overview(self, event):
         
+        
         butt_label = ""
 
-        coords = (event.x, event.y)
-        print(coords)
+        #Get the coordinates of the click
+        x = event.x
+        y = event.y
 
-        #Hardcoded :(
-        #Calibrated to match the touchscreen image coords
-        match coords:
-            #Marine Button
-            case coords as x, y if x < (self.master.winfo_width()/2) and y > (self.master.winfo_height()/2) and y < (self.master.winfo_height() * 3/4):
-                print("SCORE")
+        #Get page height and width
+        page_width = self.winfo_width()
+        page_height = self.master.winfo_height()
+
+        #Ifelse rather than match because no pattern matching, always x,y format
+
+        #Check if capabilities:
+        if y >= page_height  * 3/4:
+            butt_label = "CAPABILITIES"        
+
+
+        #Check the x coordinate
+        #Left half
+        elif x <= page_width/2:
+            #Defence Page
+            if y < page_height * 1/4:
+                butt_label = "DEFENCE"
+            #Aviation Page
+            if y > page_height * 1/4 and y < page_height / 2:
+                butt_label = "AVIATION"            
+            #Marine page
+            elif y > page_height/2:
                 butt_label = "MARINE"
-                
-            case coords as x, y if y > 200:
-                print("MISS")
-                return
-            case _:
-                return           
+
+        #Right half of the page
+        elif x > page_width/2:
+            #Medical Page
+            if y < page_height * 1/4:
+                butt_label = "MEDICAL"
+            #Industrial Page
+            if y > page_height * 1/4 and y < page_height / 2:
+                butt_label = "INDUSTRIAL"
+            #Sat comms page
+            if y > page_height/2:
+                butt_label = "SATCOM"        
+
+     
 
         
-             
-
+        print(butt_label)
 
         #Open the relevant overview page
         self.master.switch_frame(Overview_Window, arg = butt_label)
@@ -258,23 +303,61 @@ class Overview_Window(tk.Frame):
 
     #Open an overview page 
     def _open_overview(self, event):
-        
-        coords = (event.x, event.y)
+          
+        butt_label = ""
 
-        #Hardcoded :(
-        #Calibrated to match the touchscreen image coords
-        match coords:
-            case coords as x, y if y < 200:
-                print("SCORE")
-                return
-            case coords as x, y if y > 200:
-                print("MISS")
-                return
-            case _:
-                return               
+        #Get the coordinates of the click
+        x = event.x
+        y = event.y
+
+
+        
+
+        #Get page height and width
+        page_width = self.winfo_width()
+        page_height = self.master.winfo_height()
+
+
+        #Check that the click is at the top of the page
+        if y < page_height * 1/8:
+            
+            if x < page_width * 1/7:
+                butt_label = "MARINE"
+
+            elif x > page_width * 1/7 and x < page_width * 2/7:
+                butt_label = "AVIATION"
+
+            elif x > page_width * 2/7 and x < page_width * 3/7:
+                butt_label = "DEFENCE"
+
+            elif x > page_width * 3/7 and x < page_width * 4/7:
+                butt_label = "INDUSTRIAL"
+
+            elif x > page_width * 4/7 and x < page_width * 5/7:
+                butt_label = "MEDICAL"
+            
+            elif x > page_width * 5/7 and x < page_width * 6/7:
+                butt_label = "SATCOM"
+            
+            elif x > page_width * 6/7: 
+                butt_label = "CAPABILITIES"
+
+
+
+        #If not exit the function        
+        else:
+            return    
+
+        
+        
+        print(butt_label)
+
+        #Open the relevant overview page
+        #self.master.switch_frame(Overview_Window, arg = butt_label)
 
         #Open the relevant overview page
         self.master.switch_frame(Overview_Window, arg = butt_label)
+        
 
 
 
@@ -302,8 +385,8 @@ if __name__ == "__main__":
                 app._frame._next_image()
 
 
-            #If a button hasn't been pressed in 60 seconds
-            if app.get_idle_time() > 60:
+            #If a button hasn't been pressed in 60 seconds - and not currently on the start page
+            if app.get_idle_time() > 60 and not app.start_flag:
                 app.switch_frame(Start_Window)
                 start_time = time.time()           
 
